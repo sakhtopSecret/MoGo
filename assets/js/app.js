d255ccd1a92b2346579a1aba1fb5ad00de37e45f
sakhtopSecret <kulaginsergey09@gmail.com>
2022-08-04 13:13:08 +1100

    Написаны JS Скрипты ver.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,27 @@ $(function() {
     var header = $("#header"),
         introH = $("#intro").innerHeight(),
         scrollOffset = $(window).scrollTop();
+        
 
     /* Fixed Header */
     checkScroll(scrollOffset);
 
     $(window).on("scroll", function() {
+
         scrollOffset = $(this).scrollTop();
 
         checkScroll(scrollOffset);
+
     });
 
     function checkScroll(scrollOffset) {
+
         if(scrollOffset >= introH -1) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
+
     }
 
     /* Smooth scroll */
@@ -32,10 +37,13 @@ $(function() {
 
         $("#nav a").removeClass("active");
         $this.addClass("active");
+        $("#nav").removeClass("active");
+        
 
         $("html, body").animate ({
             scrollTop: blockOffset
         }, 500)
+
     });
 
     /* Menu nav togle */
@@ -45,8 +53,30 @@ $(function() {
 
         $(this).toggleClass("active");
         $("#nav").toggleClass("active");
+        
     });
 
+    /* Collapse */
+
+    $("[data-collapse").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        
+        $this.toggleClass("active");
+        
+    });
+
+    /* Slider */
+
+    $('.data-slider').slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 
 
 });
